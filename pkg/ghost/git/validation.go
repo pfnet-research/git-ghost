d841080b502b122f65288bec5630d0419a42a532
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2019-01-27 14:11:53 +0900

    Pass stderr on git command failures

--- pkg/ghost/git/validation.go
+++ pkg/ghost/git/validation.go
@@ -1,22 +1,36 @@
 package git
 
 import (
+	"bytes"
+	"errors"
 	"os/exec"
 )
 
 func ValidateGit() error {
-	gitCmd := exec.Command("git", "version")
-	err := gitCmd.Run()
+	cmd := exec.Command("git", "version")
+	stderr := bytes.NewBufferString("")
+	cmd.Stderr = stderr
+	err := cmd.Run()
 	if err != nil {
+		s := stderr.String()
+		if s != "" {
+			return errors.New(s)
+		}
 		return err
 	}
 	return nil
 }
 
 func ValidateCommitish(commitish string) error {
-	gitCmd := exec.Command("git", "cat-file", "-e", commitish)
-	err := gitCmd.Run()
+	cmd := exec.Command("git", "cat-file", "-e", commitish)
+	stderr := bytes.NewBufferString("")
+	cmd.Stderr = stderr
+	err := cmd.Run()
 	if err != nil {
+		s := stderr.String()
+		if s != "" {
+			return errors.New(s)
+		}
 		return err
 	}
 	return nil
