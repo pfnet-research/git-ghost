93dfe99db37f7c83d77067dcf4d978e29fbd1e84
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2019-01-27 17:14:38 +0900

    Add ghost branch types

--- pkg/ghost/types.go
+++ pkg/ghost/types.go
@@ -1,7 +1,51 @@
 package ghost
 
+import (
+	"fmt"
+)
+
 type Commit struct {
 	BaseCommitHash string
 	Commits        []string
 	Diff           string
+}
+
+type GhostBranch interface {
+	BranchName() string
+	FileName() string
+}
+
+type LocalBaseBranch struct {
+	Prefix           string
+	RemoteBaseCommit string
+	LocalBaseCommit  string
+}
+
+type LocalModBranch struct {
+	Prefix          string
+	LocalBaseCommit string
+	LocalModHash    string
+}
+
+func (b *LocalBaseBranch) BranchName() string {
+	return fmt.Sprintf("%s/%s-%s", b.Prefix, b.RemoteBaseCommit, b.LocalBaseCommit)
+}
+
+func (b *LocalBaseBranch) FileName() string {
+	return "commits.bundle"
+}
+
+func (b *LocalModBranch) BranchName() string {
+	return fmt.Sprintf("%s/%s/%s", b.Prefix, b.LocalBaseCommit, b.LocalModHash)
+}
+
+func (b *LocalModBranch) FileName() string {
+	return "diff.patch"
+}
+
+type PushOptions struct {
+	GhostPrefix string
+	GhostRepo   string
+	RemoteBase  string
+	LocalBase   string
 }
