a2411a92ce4106996d0d32b86a50e9acb75c96ed
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2019-02-08 18:07:55 +0900

    Add e2e test for include files

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -23,6 +23,8 @@ func TestAll(t *testing.T) {
 	defer ghostDir.Remove()
 
 	t.Run("BasicScenario", CreateTestBasicScenario(ghostDir))
+	t.Run("IncludeFile", CreateTestIncludeFile(ghostDir))
+	t.Run("IncludeLinkFile", CreateTestIncludeLinkFile(ghostDir))
 }
 
 func CreateTestBasicScenario(ghostDir *util.WorkDir) func(t *testing.T) {
@@ -86,6 +88,129 @@ func CreateTestBasicScenario(ghostDir *util.WorkDir) func(t *testing.T) {
 		// TODO: Do some assertion
 
 		// TODO: delete the ghost branches and do some assertion
+	}
+}
+
+func CreateTestIncludeFile(ghostDir *util.WorkDir) func(t *testing.T) {
+	return func(t *testing.T) {
+		srcDir, err := util.CreateGitWorkDir()
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer srcDir.Remove()
+
+		err = setupBasicGitRepo(srcDir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		srcDir.Env = map[string]string{
+			"GHOST_REPO": ghostDir.Dir,
+		}
+
+		dstDir, err := util.CloneWorkDir(srcDir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		dstDir.Env = map[string]string{
+			"GHOST_REPO": ghostDir.Dir,
+		}
+		defer dstDir.Remove()
+
+		// Make one modification
+		_, _, err = srcDir.RunCommmand("bash", "-c", "echo c > sample.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Make a file
+		_, _, err = srcDir.RunCommmand("bash", "-c", "echo 'this is an included file' > included_file")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		stdout, _, err := srcDir.RunCommmand("git", "ghost", "push", "--include", "included_file")
+		if err != nil {
+			t.Fatal(err)
+		}
+		diffHash := strings.TrimRight(stdout, "\n")
+		assert.NotEqual(t, "", diffHash)
+
+		_, _, err = dstDir.RunCommmand("git", "ghost", "pull", diffHash)
+		if err != nil {
+			t.Fatal(err)
+		}
+		stdout, _, err = dstDir.RunCommmand("cat", "included_file")
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, "this is an included file\n", stdout)
+	}
+}
+
+func CreateTestIncludeLinkFile(ghostDir *util.WorkDir) func(t *testing.T) {
+	return func(t *testing.T) {
+		srcDir, err := util.CreateGitWorkDir()
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer srcDir.Remove()
+
+		err = setupBasicGitRepo(srcDir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		srcDir.Env = map[string]string{
+			"GHOST_REPO": ghostDir.Dir,
+		}
+
+		dstDir, err := util.CloneWorkDir(srcDir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		dstDir.Env = map[string]string{
+			"GHOST_REPO": ghostDir.Dir,
+		}
+		defer dstDir.Remove()
+
+		// Make one modification
+		_, _, err = srcDir.RunCommmand("bash", "-c", "echo c > sample.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Make a file
+		_, _, err = srcDir.RunCommmand("bash", "-c", "echo 'this is an included file' > included_file")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Make a symlink to the file above
+		_, _, err = srcDir.RunCommmand("bash", "-c", "ln -ns included_file included_link")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		stdout, _, err := srcDir.RunCommmand("git", "ghost", "push", "--include", "included_link")
+		if err != nil {
+			t.Fatal(err)
+		}
+		diffHash := strings.TrimRight(stdout, "\n")
+		assert.NotEqual(t, "", diffHash)
+
+		_, _, err = dstDir.RunCommmand("git", "ghost", "pull", diffHash)
+		if err != nil {
+			t.Fatal(err)
+		}
+		stdout, _, err = dstDir.RunCommmand("cat", "included_file")
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, "this is an included file\n", stdout)
+		stdout, _, err = dstDir.RunCommmand("cat", "included_link")
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, "this is an included file\n", stdout)
 	}
 }
 
